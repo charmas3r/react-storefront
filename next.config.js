6008326bbc8ca25b2de13d0ba8a5ef86d736efa5
Evan Smith <evansmith0115@gmail.com>
2022-01-15 21:31:59 -0800

    pubic homepage under develoment banner

--- next.config.js
+++ next.config.js
@@ -6,7 +6,11 @@ module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["vercel.saleor.cloud", "img.youtube.com", "westcoastplumeria.s3.amazonaws.com"],
+    domains: [
+      "vercel.saleor.cloud",
+      "img.youtube.com",
+      "westcoastplumeria.s3.amazonaws.com",
+      "images.unsplash.com"],
     formats: ["image/avif", "image/webp"],
   },
   i18n: {
